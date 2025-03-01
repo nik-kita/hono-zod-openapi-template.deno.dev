49805fea2b37e87e0cf9b02ee489df20275ccced
nik-kita <nik@kita.github>
2025-03-01 04:26:52 +0100

    init with base min config
    1. auto pr dev=>main
    2. deploy
    3. hono
    4. zod
    5. openapi (scalar)

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
-Deno.serve((req: Request) => new Response("Hello World"));
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { apiReference } from "@scalar/hono-api-reference";
+import deno_jsonc from "./deno.json" with { type: "json" };
+
+const app = new OpenAPIHono();
+
+app.doc("/doc", {
+  openapi: "3.0.0",
+  info: {
+    version: (deno_jsonc as { version: string }).version,
+    title: "GQL <=> HTTP proxy dev converter API",
+  },
+});
+
+app.get(
+  "/api",
+  apiReference({
+    spec: {
+      url: "/doc",
+    },
+  }),
+);
+
+app.notFound((c) => {
+  return c.redirect("/api");
+});
+
+Deno.serve({
+  port: 3000,
+}, app.fetch);
